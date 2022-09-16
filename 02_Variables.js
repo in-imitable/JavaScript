210018f1e92eb682e25679b6a16f5f317b04ee19
in-imitable <atul69030@gmail.com>
2022-09-16 19:32:53 +0530

    Initial Commit

---
+++ 02_Variables.js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a)
+a = "atul"
+console.log(a)
+// let 8atul = 7 // Not allowed this will throw an error
+// let var = 7
---
+++ 04_PrimitiveObjects.js
@@ -0,0 +1,10 @@
+//There is 7 types of primitive data types
+let a = null
+let b = 35
+let c = "Atul"
+let d = Symbol("This is a symbol")
+let e = true
+let f = BigInt(654)
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof(f))
