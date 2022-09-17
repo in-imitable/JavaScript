84e63fc93eb9796edb944f843fcfcbd43a84f43b
in-imitable <atul69030@gmail.com>
2022-09-17 15:42:03 +0530

    while & do-while loops implementation

---
+++ Chapter 3/10_while_loops.js
@@ -0,0 +1,22 @@
+// while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+
+
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+do{
+  console.log(i)
+  i++;
+}while (i < n) 
