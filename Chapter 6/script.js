0f2fcfb0f4b5be011fc99a1bdb2a41672ae0c425
in-imitable <atul69030@gmail.com>
2022-09-21 17:42:02 +0530

    Console Object Methods

--- Chapter 6/script.js
+++ Chapter 6/script.js
@@ -1,2 +1,28 @@
 // alert("Hello")
 console.log("Hey Atul")
+
+// Console Object Methods
+console.log("log")
+console.info("This is an info")
+console.warn("Hey this is a warning")
+console.error("error")
+console.assert("error" != false)
+console.assert("error" == false)
+
+console.time("forLoop")
+
+for (let i = 0; i < 500; i++) {
+  console.log(233)
+}
+
+console.timeEnd("forLoop")
+
+console.time("whileLoop")
+
+let i = 0;
+while (i < 500) {
+  console.log(233)
+  i++;
+}
+
+console.timeEnd("whileLoop")
