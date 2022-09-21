00122e12161986d00cee791d533f1247499497a2
in-imitable <atul69030@gmail.com>
2022-09-21 21:18:15 +0530

    Chapter 6 - Q3

--- Chapter 6/Practice Set/29_PracticeSet.js
+++ Chapter 6/Practice Set/29_PracticeSet.js
@@ -8,6 +8,11 @@ while(runAgain){
     let age = prompt("Enter your age:")
     age = Number.parseInt(age)
 
+    if(age<0){
+        console.error("User entered the negative value. Please enter the valid age.")
+        break;
+    }
+
     if(canDrive(age)){
         alert("You can drive")
     }
