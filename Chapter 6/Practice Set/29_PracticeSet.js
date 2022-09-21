8e67895694c74bf7dee4cf79c63836f841b236e6
in-imitable <atul69030@gmail.com>
2022-09-21 21:37:10 +0530

    Chapter 6 - Q5

--- Chapter 6/Practice Set/29_PracticeSet.js
+++ Chapter 6/Practice Set/29_PracticeSet.js
@@ -26,9 +26,15 @@ while(runAgain){
 */
 
 // Q4
+/*
 let number = prompt("Enter your number:")
 number = Number.parseInt(number)
 
 if(number>4){
     location.href = "https://google.com"
 }
+*/
+
+//Q5
+let color = prompt("Enter the page background color:")
+document.body.style.background = color
