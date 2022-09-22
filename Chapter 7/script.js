161c27dd22ccff0f220a7471d2450536c6e2f7d5
in-imitable <atul69030@gmail.com>
2022-09-22 20:49:42 +0530

    Parents and the Siblings of the Document

--- Chapter 7/script.js
+++ Chapter 7/script.js
@@ -1,8 +1,17 @@
 //31 Walking the DOM
 
+/*
 //32 Children of an element
 console.log(document.body.firstChild)
 console.log(document.body.lastChild)
 // console.log(document.body.childNodes) //This will not array, they are nodes
 let arr = Array.from(document.body.childNodes)
 console.log(arr)
+*/
+
+// 33 Parents and Siblings
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
