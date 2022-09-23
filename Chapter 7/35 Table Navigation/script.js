878658234d167b4963b426f8f4684afcc3302db0
in-imitable <atul69030@gmail.com>
2022-09-23 13:53:42 +0530

    Table Navigation Manipulation

---
+++ Chapter 7/35 Table Navigation/script.js
@@ -0,0 +1,7 @@
+let table = document.body.firstElementChild.firstElementChild
+console.log(table)
+console.log(table.rows)
+console.log(table.caption)
+console.log(table.tHead)
+console.log(table.tFoot)  //null
+console.log(table.tBodies)
