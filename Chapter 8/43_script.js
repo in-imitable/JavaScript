6a8f12cd2a3a29aebb89fb3f3e3d143401299950
in-imitable <atul69030@gmail.com>
2022-09-24 14:03:34 +0530

    Insertion Methods

---
+++ Chapter 8/43_script.js
@@ -0,0 +1,13 @@
+//Insertion Method
+
+let a = document.getElementsByTagName('div')[0]
+// console.log(a)
+// a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
+
+let div = document.createElement('div');
+div.innerHTML = '<h1>Hello World!</h1>';
+// a.append(div);
+// a.prepend(div);
+// a.before(div);
+// a.after(div);
+// a.replaceWith(div);
