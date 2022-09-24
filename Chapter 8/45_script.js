c4b561641aa51814302eec0945119e82465c6cfe
in-imitable <atul69030@gmail.com>
2022-09-24 16:01:38 +0530

    className & classList

---
+++ Chapter 8/45_script.js
@@ -0,0 +1,7 @@
+//className & classList
+
+first.className = "text-black red"
+first.classList.add("red")
+first.classList.remove("red")
+first.classList.contains("red")
+first.classList.toggle("red")
